a26f057b4772676cc185f452309faada12b76ed6
petekal1323 <petekal1323@gmail.com>
2025-08-04 18:03:30 -0400

    exercises

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
 
-// Exercise 2
 
-// Exercise 3
+const firstName = "John"
+let lastName = "Doe"
+var age = 30;
+
+
+lastName = "";
+age = 18;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}`);
+
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} is available in size ${selectedSize} and is priced at $${price}.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} has ${pageCount} pages and is currently at page ${bookmark}.`);
